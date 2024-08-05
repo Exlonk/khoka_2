bc2ecee5877b3bd6b03fe0c811a331856a0beeaa
Exlonk <81200520+Exlonk@users.noreply.github.com>
2024-08-05 20:40:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
          meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"},
     ])
-
+server = app.server
 # Define the navigation bar with the dropdown menu
 
 header = [
