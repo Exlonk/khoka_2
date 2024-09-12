99d09693f0b31743efdd4be4f06bb0fed837f10d
Exlonk <81200520+Exlonk@users.noreply.github.com>
2024-09-12 21:13:59 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -19,38 +19,45 @@ nav= dbc.Nav(
                     [
                         dbc.DropdownMenu(
                             [
-                                dbc.DropdownMenuItem("¿Qué es la metabolómica?", header=False),
-                                dbc.DropdownMenuItem("Metabolismo y Metabolitos en Plantas", href="#"),
-                                dbc.DropdownMenuItem("Metabolitos", href="#"),
-                                dbc.DropdownMenuItem("Metabolismo primario", href="#"),
-                                dbc.DropdownMenuItem("Metabolismo secundario", href="#"),
-                                dbc.DropdownMenuItem("Metabolómica en la investigación de la coca", href="#"),
+                                dbc.DropdownMenuItem("¿Qué es la metabolómica?", header=False,
+                                                     href=f"#intro_metabol_1",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Metabolismo y Metabolitos en Plantas", 
+                                                     href=f"#intro_metabol_2",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Metabolitos", 
+                                                     href=f"#intro_metabol_3",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Metabolismo primario", 
+                                                     href=f"#intro_metabol_p",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Metabolismo secundario",
+                                                     href=f"#intro_metabol_s",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Metabolómica en la investigación de la coca", 
+                                                     href=f"#intro_metabol_4",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
                             ],
                             nav=True,
                             in_navbar=True,
-                            label=dbc.NavLink("INTRODUCCIÓN",style={'padding':0}),
+                            label=dbc.NavLink("INTRODUCCIÓN", href="/",style={'padding':0}),
                         ),
                         dbc.DropdownMenu(
                             [
                                 dbc.DropdownMenuItem("PASO 1: Extracción de los compuestos presentes en las hojas", header=False,
-                                                     style={'fontSize': '1rem'}, href=f"#experimento_paso_1",external_link=True),
-                                dbc.DropdownMenuItem("Principios básicos de extracción química", href=f"#pbe",external_link=True),
-                                dbc.DropdownMenuItem("Extracción líquido-líquido simple", href=f"#extraccion_liquido",external_link=True),
-                                dbc.DropdownMenuItem("Características del disolvente de extracción", href=f"#car_sol",external_link=True),
-                                dbc.DropdownMenuItem("PASO 2: Análisis de compuestos: Cromatografía de Gases", href=f"#experimento_paso_2",external_link=True),
-                                dbc.DropdownMenuItem("¿Qué es una cromatografía?", href="#que_es_cro",external_link=True),
+                                                     style={"fontFamily":"basker_120","fontSize":"1rem"}, href=f"#experimento_paso_1",external_link=True),
+                                dbc.DropdownMenuItem("Principios básicos de extracción química", href=f"#pbe",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Extracción líquido-líquido simple", href=f"#extraccion_liquido",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Características del disolvente de extracción", href=f"#car_sol",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("PASO 2: Análisis de compuestos: Cromatografía de Gases", href=f"#experimento_paso_2",external_link=True,style={"fontFamily":"basker_120","fontSize":"1rem"}),
+                                dbc.DropdownMenuItem("¿Qué es una cromatografía?", href="#que_es_cro",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
                             ],
                             nav=True,
                             in_navbar=True,
-                            label=dbc.NavLink("EXPERIMENTO", href="/",style={'padding':0}),
+                            label=dbc.NavLink("EXPERIMENTO", href="/khoka-experiment", style={'padding':0}),
                             
                             
                         ),
                         dbc.DropdownMenu(
                             [
-                                dbc.DropdownMenuItem("MAPA DE METABOLITOS DE LA FAMILIA DE LA COCA", header=False,
-                                                     style={'fontSize': '1rem'}, href=f"#experimento_paso_1",external_link=True),
-                                dbc.DropdownMenuItem("CARACTERIZACIÓN QUÍMICA POR VARIEDADES", href=f"#pbe",external_link=True)
+
+                                dbc.DropdownMenuItem("CARACTERIZACIÓN QUÍMICA POR VARIEDADES",style={"fontFamily":"basker_120","fontSize":"0.8rem"}, href=f"#caracterizacion",external_link=True),
+                                dbc.DropdownMenuItem("CARACTERIZACIÓN QUÍMICA POR VARIEDADES",style={"fontFamily":"basker_120","fontSize":"0.8rem"}, href=f"#compuestos_bioactivos",external_link=True),
+                                dbc.DropdownMenuItem("CROMATOGRAMAS",style={"fontFamily":"basker_120","fontSize":"0.8rem"}, href=f"#cromatogramas",external_link=True)
 
                             ],
                             nav=True,
@@ -58,9 +65,25 @@ nav= dbc.Nav(
                             label=dbc.NavLink("RESULTADOS", href="/khoka-results",style={'padding':0}),    
                         ),
 
-                        dbc.NavItem(dbc.NavLink("METODOLOGÍA", href="#")),
+                        dbc.DropdownMenu(
+                            [
+                                dbc.DropdownMenuItem("Cromatografía de gases", header=False,
+                                                    href=f"#meto_croma",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"}),
+                                dbc.DropdownMenuItem("Tiempo de Retención", href=f"#meto_tiempo",external_link=True,style={"fontFamily":"basker_120","fontSize":"0.8rem"})
+
+                            ],
+                            nav=True,
+                            in_navbar=True,
+                            label=dbc.NavLink("METODOLOGÍA", href="/khoka_methodology",style={'padding':0}),    
+                        ),
+#                        dbc.NavItem(dbc.NavLink("METODOLOGÍA", href="/khoka_methodology",style={'padding':0})),
                         dbc.NavItem(dbc.NavLink("CONCLUSIONES", href="#")),
-                        dbc.NavItem(dbc.NavLink("GLOSARIO", href="#")),
+                        dbc.DropdownMenu(
+                            nav=True,
+                            in_navbar=True,
+                            label=dbc.NavLink("GLOSARIO", href="/khoka_glossary",style={'padding':0}),    
+                        ),
+#                        dbc.NavItem(dbc.NavLink("GLOSARIO", href="#")),
                         dbc.NavItem(dbc.NavLink("BIBLIOGRAFÍA", href="#")),
                     ],
                     style={'fontFamily': 'clutadella_light','backgroundColor': '#9abf99'},
@@ -99,5 +122,5 @@ for page in dash.page_registry.values():
     print('url:', page['path'])
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
-
+    app.run_server(
+        debug=True)
